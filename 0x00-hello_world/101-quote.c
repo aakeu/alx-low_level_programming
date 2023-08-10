e508c9613e65e36a14433bff06aa387f9474ff62
aakeu <ibrahimakeukereke@outlook.com>
2023-08-10 14:20:53 -0700

    y

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,11 @@
 /**
  * main - Entry point takes parameter
  * Return: Always 0 Indicates success
-*/
+ */
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, quo, 59);
-return (0);
+	char fwrite[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(1, quo, 59);
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,17 @@
 /**
  * main - Entry point takes parameter
  * Return: Always 0 Indicates success
-*/
+ */
 int main(void)
 {
-int i;
-char c;
-float f;
+	int i;
+	char c;
+	float f;
 
-printf("Size of a char: %u bytes(s)\n", sizeof(c));
-printf("Size of an int: %u bytes(s)\n", sizeof(i));
-printf("Size of a long int: %u bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: %u bytes(s)\n", sizeof(long long int));
-printf("Size of a float: %u bytes(s)\n", sizeof(f));
-return (0);
+	printf("Size of a char: %u bytes(s)\n", sizeof(c));
+	printf("Size of an int: %u bytes(s)\n", sizeof(i));
+	printf("Size of a long int: %u bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %u bytes(s)\n", sizeof(f));
+	return (0);
 }
