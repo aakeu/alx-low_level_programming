693c8f1e8dec049ad69b53223f16584f96d0c669
aakeu <ibrahimakeukereke@outlook.com>
2023-08-10 13:54:10 -0700

    y

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point takes parameter
+ * Return: Always 0 Indicates success
+*/
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
+return (0);
