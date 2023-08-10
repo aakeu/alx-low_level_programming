0483ed7ca3565868be009066d5c9227a1fdf981a
aakeu <ibrahimakeukereke@outlook.com>
2023-08-10 12:09:09 -0700

    long long int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,13 @@ int main(void)
 	char a;
 	int b;
 	long int c;
-	long long int;
+	long long int d;
 	float f;
 
-	printf("size of a char: %lu bytes(s)\n", sizeof(char));
-	printf("size of an int: %lu bytes(s)\n", sizeof(int));
-	printf("size of a long int: %lu bytes(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("size of a char: %lu bytes(s)\n", sizeof(char(a));
+	printf("size of an int: %lu bytes(s)\n", sizeof(int(b));
+	printf("size of a long int: %lu bytes(s)\n", sizeof(long int(c));
+	printf("size of a long long int: %lu bytes(s)\n", sizeof(long long int(d));
+	printf("size of a float: %lu bytes(s)\n", sizeof(float(f));
 	return (0);
 }
