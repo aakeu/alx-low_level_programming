827202768356b57a8851c24c53cd019f988634fc
aakeu <ibrahimakeukereke@outlook.com>
2023-08-10 16:01:32 -0700

    using write to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point takes parameter
  * Return: Always 0 Indicates success
  */
 int main(void)
 {
-	fprintf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-i
-	fprintf(1, quo, 59);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 590);
 	return (1);
 }
