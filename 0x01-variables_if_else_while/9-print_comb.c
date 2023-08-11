e6b72f112faa0d9bc1e6b181496b556e37a8d9ea
aakeu <ibrahimakeukereke@outlook.com>
2023-08-11 15:24:11 -0700

    ramdon numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,54 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Entry point of thr program
- *
- * Description: This code will write 1 to 9 in order
- * with spaceZZ.
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
 	{
-		putchar('0');
-		putchar(',');
-		putchar(' ');
+		putchar(digit + 48);
 
-		putchar('1');
-		putchar(',');
-		putchar(' ');
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 
-		putchar('2');
-		putchar(',');
-		putchar(' ');
+		digit++;
+	}
 
-		putchar('3');
-		putchar(',');
-		putchar(' ');
-
-		putchar('4');
-		putchar(',');
-		putchar(' ');
-
-		putchar('5');
-		putchar(',');
-		putchar(' ');
-
-		putchar('6');
-		putchar(',');
-		putchar(' ');
-
-		putchar('7');
-		putchar(',');
-		putchar(' ');
-
-		putchar('8');
-		putchar(',');
-		putchar(' ');
-
-		putchar('9');
-		putchar('\n');
-
-		return 0;
-	
+	putchar('\n');
+	return (0);
+}
