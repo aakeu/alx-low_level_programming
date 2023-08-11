7bc55afbb01840d4a04c410c7964f6dff4d71c2f
aakeu <ibrahimakeukereke@outlook.com>
2023-08-11 09:22:17 -0700

    ramdon numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,23 +15,25 @@
 int main(void)
 {
 	int n;
+	int y;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	n = n % 10;
+	printf("Last digit of %d is ", n);
+	y = n % 10;
 
-	if (n > 5)
+	printf("%d and ", y);
+	if (y > 5)
 	{
-		printf("and is greater than 5");
+		printf("is greater than 5\n");
 	}
-	else if (n == 0)
+	else if (y == 0)
 	{
-		printf("and is 0");
+		printf("is 0\n");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0");
+		printf("is less than 6 and not 0\n");
 	}
-	printf("\n");
 	return (0);
 }
