59f0d6e0471f53d0506e7b14952cc5ebb4ea56c5
aakeu <ibrahimakeukereke@outlook.com>
2023-08-11 14:15:49 -0700

    y

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of thr program
+ *
+ * Description: This code will write 1 to 9 in order
+ * with spaceZZ.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		putchar('0');
+		putchar(',');
+		putchar(' ');
+
+		putchar('1');
+		putchar(',');
+		putchar(' ');
+
+		putchar('2');
+		putchar(',');
+		putchar(' ');
+
+		putchar('3');
+		putchar(',');
+		putchar(' ');
+
+		putchar('4');
+		putchar(',');
+		putchar(' ');
+
+		putchar('5');
+		putchar(',');
+		putchar(' ');
+
+		putchar('6');
+		putchar(',');
+		putchar(' ');
+
+		putchar('7');
+		putchar(',');
+		putchar(' ');
+
+		putchar('8');
+		putchar(',');
+		putchar(' ');
+
+		putchar('9');
+		putchar('\n');
+
+		return 0;
+	
