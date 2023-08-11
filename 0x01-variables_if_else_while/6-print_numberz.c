d4f5fe7efd43430a263270b1a0c6443c1e311359
aakeu <ibrahimakeukereke@outlook.com>
2023-08-11 15:41:04 -0700

    ramdon numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,17 +11,13 @@
  */
 int main(void)
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
-putchar('\n');
+	int digit = 0;
 
-return (0);
+	while (digit <= 9)
+	{putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
 }
