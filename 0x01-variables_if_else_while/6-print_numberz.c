6a68314717ee4ef8a7682d88e3f1b3b13e65a7c9
aakeu <ibrahimakeukereke@outlook.com>
2023-08-11 12:34:32 -0700

    digit 9 to 1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of thr program
+ *
+ * Description: This spell out all digit 9 to 0
+ * positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar('0' + 0);
+putchar('0' + 1);
+putchar('0' + 2);
+putchar('0' + 3);
+putchar('0' + 4);
+putchar('0' + 5);
+putchar('0' + 6);
+putchar('0' + 7);
+putchar('0' + 8);
+putchar('0' + 9);
+putchar('\n');
+
+return (0);
+}
