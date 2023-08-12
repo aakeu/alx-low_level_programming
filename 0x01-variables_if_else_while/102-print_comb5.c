19da48bd2852186715487f7dbd3b6c6033f2a0d0
aakeu <ibrahimakeukereke@outlook.com>
2023-08-11 23:31:40 -0700

    advance task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of thr program
+ *
+ * Description: This code print double digit combination
+ * from 0 to 99. They are the smallest combination.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first_digit, second_digit;
+
+	for (first_digit = 0; first_digit <= 99; first_digit++)
+	{
+		for (second_digit = 0; second_digit <= 99; second_digit++)
+		{
+			if (first_digit <= second_digit)
+			{
+				putchar(first_digit / 10 + '0');
+				putchar(first_digit % 10 + '0');
+				putchar(' ');
+				putchar(second_digit / 10 + '0');
+				putchar(second_digit % 10 + '0');
+
+				if (first_digit != 99 || second_digit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}	}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
