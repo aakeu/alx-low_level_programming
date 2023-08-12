407ce93614f33116988e5eb111652bb5d89d7935
aakeu <ibrahimakeukereke@outlook.com>
2023-08-12 01:34:57 -0700

    numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,29 +11,31 @@
  */
 int main(void)
 {
-	int num1, num2;
+    int num1 = 0, num2;
 
-	for (num1 = 0; num1 <= 99; num1++)
-	{
-		for (num2 = 1; num2 <= 99; num2++)
-		{
-			if (num1 <= num2)
-			{
-				putchar(num1 / 10 + '0');
-				putchar(num1 % 10 + '0');
-				putchar(' ');
-				putchar(num2 / 10 + '0');
-				putchar(num2 % 10 + '0');
+    while (num1 <= 99)
+    {
+        num2 = num1;
+        while (num2 <= 99)
+        {
+            if (num2 != num1)
+            {
+                putchar((num1 / 10) + 48);
+                putchar((num1 % 10) + 48);
+                putchar(' ');
+                putchar((num2 / 10) + 48);
+                putchar((num2 % 10) + 48);
 
-				if (num1 != 99 || num2 != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-
-	putchar('\n');
-	return (0);
+                if (num1 != 98 || num2 != 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+            num2++;
+        }
+        num1++;
+    }
+    putchar('\n');
+    return (0);
 }
