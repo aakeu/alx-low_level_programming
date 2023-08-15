5ce61eb037b9cb30a9e802846e5bedd7c09b4d16
aakeu <ibrahimakeukereke@outlook.com>
2023-08-15 12:41:27 -0700

    integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - print integer
+ * @n: Integer %d input
+ * Return: boolean vaule
+ */
+int _abs(int n)
+{
+	int r;
+
+	if (n >= 0)
+	{
+		r = n;
+	}
+	else
+	{
+		r = -n;
+	}
+
+	return (abs);
+}
