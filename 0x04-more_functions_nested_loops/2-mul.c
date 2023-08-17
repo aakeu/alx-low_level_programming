02eb85e49217538294c3eaab9d14f683a2b4262b
aakeu <ibrahimakeukereke@outlook.com>
2023-08-17 09:16:20 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul- function that multiply two integer
+ *
+ * @a: number 1 for mutliplication
+ *
+ * @b: number 2 for mutiplication
+ *
+ * Return: Result returned.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers- print out numbers 0 to 9
+ *
+ * @Void: it is printing out
+ *
+ * Return: 0 return
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar (i + 48);
+		putchar ('\n');
+	}
+}
