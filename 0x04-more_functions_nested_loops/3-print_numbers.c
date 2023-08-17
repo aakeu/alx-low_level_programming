6c2c31001573632adea44266461623956672a4e8
aakeu <ibrahimakeukereke@outlook.com>
2023-08-17 09:19:59 -0700

    0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,9 +12,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar (i + 48);
-		putchar ('\n');
+		_putchar (i + 48);
+		_putchar ('\n');
 	}
 }
