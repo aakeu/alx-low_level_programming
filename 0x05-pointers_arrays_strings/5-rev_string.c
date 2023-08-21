7ff8fe11d9330f6e67e129f62a07e6b1abce29a3
aakeu <ibrahimakeukereke@outlook.com>
2023-08-21 04:23:24 -0700

    school

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,22 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * rev_string-
+ * rev_string-it is meant to reverse the whole string
  *
- * @s:
+ * @s:string that is the parameter input
  *
  * Return: Null
-*/
+ */
 
 void rev_string(char *s)
 {
 	int l = strlen(s);
-	int a; 
+	int a;
 	int b;
 
 	for (a = 0, b = l - 1; a < b; a++, b--)
+
 	{
 		char temp = s[a];
+
 		s[a] = s[b];
 		s[b] = temp;
 	}
