20ac36fbe1d2fd22d8b2116f240c1a91cf3041f9
aakeu <ibrahimakeukereke@outlook.com>
2023-08-21 06:38:32 -0700

    every other number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * put2- putchar out characters on the other line
+ *
+ * @str: strings that input the parameter
+ *
+ * Return: Null
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; ++i)
+
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+
+}
