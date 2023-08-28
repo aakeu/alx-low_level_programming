c5c0205c2897fbb067394e4d52d018e4d2d1f4d8
aakeu <ibrahimakeukereke@outlook.com>
2023-08-28 07:33:13 -0700

    correction

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,9 +11,9 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	while(*s != '\0')
+	while (*s != '\0')
 	{
-		if (strchr(accept, *s) != NULL) 
+		if (strchr(accept, *s) != NULL)
 		{
 			return (s);
 		}
