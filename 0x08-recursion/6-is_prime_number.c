9ed14e30900e42e15bfd9fcc0de914bd3ba348f2
aakeu <ibrahimakeukereke@outlook.com>
2023-08-30 03:57:11 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,29 @@
 /**
  *is_prime_number- return of prime number
  *@n: an int
+ *@dividor: int the divide
  *Return: Nothing
-*/
+ */
 
+int check_prime(int n, int p);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
 
+/**
+ * check_prime- check the int  is < n if we can divide it
+ * @n: int
+ * @p: int
+ * Return: int
+ */
+
+int check_prime(int n, int p)
+{
+	if (p >= n && n > 1)
+		return (1);
+	else if (n % p == 0 || n <= 1)
+		return(0);
+	else
+		return (check_prime(n, p + 1));
+}
