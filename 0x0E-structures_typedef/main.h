edfb2988bd9e4f02cfbbd80315619e43b9f9684a
aakeu <ibrahimakeukereke@outlook.com>
2023-09-11 06:58:42 -0700

    struc

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
-int _putchar(char c);;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 
 #endif /* MAIN H */
