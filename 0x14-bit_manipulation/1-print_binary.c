748af8decd06860013ffeabaab018831f7f64aaf
aakeu <ibrahimakeukereke@outlook.com>
2023-09-28 12:38:01 -0700

    update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int in, counter = 0;
+	unsigned long int current;
+
+	for (in = 63; in >= 0; in--)
+	{
+		current = n >> in;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			counter++;
+		}
+		else if (counter)
+			_putchar('0');
+	}
+	if (!counter)
+		_putchar('0');
+}
